4fcf06c4c25ab1a1d9f47a6db648b6af3e028784
Kissanth <dkissanth1@gmail.com>
2021-03-05 22:19:06 +0530

    add the message

--- survey/settings.py
+++ survey/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
+import django_heroku
 import os
 from pathlib import Path
 
@@ -121,3 +122,4 @@ STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
+django_heroku.settings(locals())
