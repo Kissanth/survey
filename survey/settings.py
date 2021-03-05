133b3a347519952ed825efdcf80b3fda50053996
Kissanth <dkissanth1@gmail.com>
2021-03-05 21:54:08 +0530

    add the secret key value

--- survey/settings.py
+++ survey/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '^6zh-)9ekuu^+x!%252#84)8ubo9u*bmhyld!jiughxg$w0cu6'
-
+#SECRET_KEY = '^6zh-)9ekuu^+x!%252#84)8ubo9u*bmhyld!jiughxg$w0cu6'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://mysurveydjangoapp.herokuapp.com/']
 
 
 # Application definition
